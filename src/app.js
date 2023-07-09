934f1716c5a06d678a07d7eb2ae9034a4ad3d2aa
EmLu26 <133606715+EmLu26@users.noreply.github.com>
2023-07-09 17:06:12 +0100

    Removed console log

--- src/app.js
+++ src/app.js
@@ -68,7 +68,6 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "eb9542c65e739e0fb25ade97c749e2aa";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
